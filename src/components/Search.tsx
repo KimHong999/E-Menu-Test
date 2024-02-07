53407e8b9ade4ecdbb9eba8745e7f42131f66ba5
KimHong999 <khong67663@gmail.com>
2024-02-07 13:46:25 +0700

    UI header

---
+++ src/components/Search.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Search = () => {
+  return (
+    <div className=" sticky top-16 z-1">
+      <form >
+        <label className="mb-2 text-sm font-medium text-gray-900 sr-only">
+          Search
+        </label>
+        <div >
+          <input
+            type="search"
+            id="default-search"
+            className=" w-full p-4 ps-10 text-sm text-gray-900  border-b border-gray-300 bg-white"
+            placeholder="Search by Category/Item Name..."
+            required
+          />
+
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Search;
---
+++ src/components/layout/NavBar.tsx
@@ -0,0 +1,109 @@
+import Link from "next/link";
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <nav className="bg-white border-gray-200">
+      <div className="flex-no-wrap fixed top-0 z-10 flex w-full items-center justify-between bg-white py-2 shadow-md shadow-black/5lg:flex-wrap lg:justify-start lg:py-2">
+        <div className="flex w-full items-center justify-between mx-10">
+          <a
+            href="https://flowbite.com/"
+            className="flex items-center space-x-3 rtl:space-x-reverse"
+          >
+            <img
+              src="https://dkxj8skx6o8xc.cloudfront.net/clouddb/Do4OzB4MOYbX/Store/oo-logo_1666065953_66.png"
+              className="h-16"
+              alt="Flowbite Logo"
+            />
+            <span className="self-center text-2xl  whitespace-nowrap ">
+              Café Chiet-Royal Palace
+            </span>
+          </a>
+          <button
+            data-collapse-toggle="navbar-default"
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+            aria-controls="navbar-default"
+            aria-expanded="false"
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
+            </svg>
+          </button>
+          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
+              <li>
+                <Link
+                  href="#"
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 "
+                >
+                  Collection
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#"
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 "
+                >
+                  Store's Info
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#"
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 "
+                >
+                  Branches
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#"
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 "
+                >
+                  Log In/Sign Up
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#"
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 "
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="file: h-6 w-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+                    />
+                  </svg>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
